ab23380c594540d1cc8b84f925d0b8756a178e93
jonathashenrique7 <grinncode@gmail.com>
2024-02-24 14:56:15 -0300

    Add => background-image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,26 @@
 import styled from "styled-components"
 import theme from "../typography/theme"
 
+import { ImageWithSpace } from "../src/components/layout/ImageWithSpace/ImageWithSpace"
+
 
 const StyledDiv = styled.div`
-  background-color: lightblue;
-  /* padding: 10px; */
+  
+  background-color: yellow;
+  
+  * {
+    margin: 0;
+    padding: 0;
+  }
 `
 
 export default function HomePage () {
   return (
-    <>
+    <ImageWithSpace>
       <StyledDiv>
-        TESTE
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, excepturi unde? Soluta ipsam dolores aliquid numquam eos rem cum voluptates officia ut at odit enim, non culpa commodi id quibusdam.
       </StyledDiv>
-    </>
+    </ImageWithSpace>
   )
 }
 
---
+++ src/components/layout/ImageWithSpace/ImageWithSpace.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+
+const StyledFlex = styled.div`
+    display: flex;
+`
+
+const StyledImage = styled.div`
+    background-image: url('${props => props.image}');
+    background-repeat: no-repeat;
+    background-position: right;
+    background-size: cover;
+    width: 100%;
+    min-height: 100vh;
+`
+
+export const ImageWithSpace = ({ children, image }) => {
+    return (
+        <StyledFlex>
+            {children}
+            <StyledImage image={image} />
+        </StyledFlex>
+    )
+}
+
+ImageWithSpace.defaultProps = {
+    image: '/background.png'
+}
